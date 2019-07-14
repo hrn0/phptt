7e4815d2369f8d93881230f0ef50a7a86deb5f36
hrn0 <51802564+hrn0@users.noreply.github.com>
2019-07-14 21:24:27 +0900

    Create a.php

---
+++ a.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "hello from the TOP!";
+?>
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>Hello World <?php echo "from php"; ?></p>
+
+  </body>
+</html>
